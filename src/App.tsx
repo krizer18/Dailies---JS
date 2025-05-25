adf302437730f0e1e381fed0b22ca672cffc03c3
sharmaji1804 <kabirsh2004@gmail.com>
2025-05-25 18:20:48 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
 import { useState } from "react";
-import countries from "../countries.json"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { onclickprompt } from "./backend/gpt";
 
 
-function randcount(){
-    const randomi = Math.floor(Math.random() * countries.length);
-    return countries[randomi];
-}
-
-
-
-export function onclickprompt(){
-    console.log("calling the api")
-}
 
 
 
 export function App(){
-    const [country, setCountry] = useState(() => randcount());
     const [question, setquestion] = useState("");
+    const [ans, setans] = useState("");
+
     return ( <div className="countPk">
 
-        <h1><strong>{country.name}</strong> and the code is {country.code} </h1>
-
-        <textarea onChange = {(e) => setquestion(e.target.value)}className="form-control" placeholder="place your question here"></textarea>
+        <textarea onChange = {(e) => setquestion(e.target.value)} className="form-control" placeholder="place your question here"></textarea>
 
         <div>
-            <button onClick = {onclickprompt} type= "button" className="btn btn-success">
+            <button onClick = {async () => { setans(await onclickprompt(question));}}type= "button" className="btn btn-success">
                 submit question
             </button>
         </div>
+        <div className="box">The answer to the question is: ${ans}</div>
     </div>
     
 
--- src/backend/gpt.tsx
+++ src/backend/gpt.tsx
@@ -1,29 +1,88 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
-import axios from "axios";
 import dotenv from "dotenv";
 import OpenAI from "openai";
+import countries from "../../countries.json"
 
+//random country generator
+export function randcount(){
+    const randomi = Math.floor(Math.random() * countries.length);
+    return countries[randomi].name;
+}
+
+//generate random country
+const country = randcount();
+
+
+//setting up the app
 dotenv.config();
 
 const port = 8000;
 const app = express();
 
-app.listen(port, () => console.log('Backend is running on ${port}'))
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const key = process.env.OpenAi_apikey;
 
-export async function onclickprompt(){
-    const api_body = {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/json",
-            "Authorization" : "Bearer" + key
-        }
-    }
 
-    await fetch("https://api.openai.com/v1/responses")
+const ai = new OpenAI({
+    apiKey: key
+})
+
+
+export async function onclickprompt(question: string) {
+    app.post("/api/prompt", async(req, res) => {
+    const {prompt} = req.body;
+    
+    try{
+    const response = await ai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+       {
+      "role": "system",
+      "content": [
+        {
+          "type": "text",
+          "text": "`You are given two words to answer with those being yes or no you may not respond with any other words. The questions you answer are based on the country: ${country} flag. Only answer questions related to the flag. If the question does not relate to the country flag respond with: \"this question does not pertain to the country's flag.`"
+        }
+    ]
+    },
+      {
+        "role" : "assistant",
+        "content" : [
+            {
+                "type" : "text",
+                "text" : `${country}`
+            }
+        ]
+      },
+      {
+        "role" : "user",
+        "content" : [
+            {
+                "type" : "text",
+                "text" : `${question}`
+            }
+        ]
+      }
+    ],
+    response_format: {
+        "type": "text"
+    },
+    temperature: 1,
+    max_completion_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+    }
+    )}
+    catch(err){
+        res.status(500).send(err);
+    }
+    })
+
 }
+app.listen(port, () => console.log(`Backend is running on ${port}`));
